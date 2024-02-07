d1fa9e2fd193c91828a93ecfa66e62689439d2da
rmxway <rmxway@mail.ru>
2024-02-07 12:06:13 +0300

    Fixed fetch products from dev and prod

--- app/product/[id]/getProduct.ts
+++ app/product/[id]/getProduct.ts
@@ -1,11 +1,11 @@
 import { productsUrl } from '@/services';
 import { domainData } from '@/services/domainData';
 
-export const getProducts = async () => {
+export const getProduct = async (id: string) => {
 	const { http, host } = await domainData();
 
 	try {
-		const res = await fetch(`${http}://${host}${productsUrl}`);
+		const res = await fetch(`${http}://${host}${productsUrl}/${id}`);
 		if (res.ok) return res.json();
 	} catch (error) {
 		// eslint-disable-next-line no-console
