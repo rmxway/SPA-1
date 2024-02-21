eb0b05b46f8e10186afc685a3ceca6cc0f163be7
rmxway <rmxway@mail.ru>
2024-02-22 00:35:42 +0300

    Added redux-persist #2

--- store/index.ts
+++ store/index.ts
@@ -1,9 +1,23 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
 
 import CartReducer from '@/store/reducers/cart';
 import ProductsReducer from '@/store/reducers/products';
+
+const createNoopStorage = () => ({
+	getItem(key: string): Promise<string | null> {
+		return Promise.resolve(key);
+	},
+	setItem(key: string, item: string): Promise<string> {
+		return Promise.resolve(item);
+	},
+	removeItem() {
+		return Promise.resolve();
+	},
+});
+
+const storage = typeof window !== 'undefined' ? createWebStorage('local') : createNoopStorage();
 
 const persistConfig = {
 	key: 'wholeStore',
