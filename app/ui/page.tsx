c4be436049a357b4208b8f8961e4c688e4bebf84
rmxway <rmxway@mail.ru>
2025-05-27 11:15:54 +0300

    Updated packages and mini fixes

--- app/ui/page.tsx
+++ app/ui/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
-import { Container, Flexbox, LayerBlock } from '@/components/Layout';
-import { Sticker } from '@/components/ui';
+import { Container, Flexbox, Grid, LayerBlock } from '@/components/Layout';
+import ProductCard from '@/components/ProductCard';
+import { mockProduct } from '@/components/ProductCard/mockProduct';
+import { Button, Sticker } from '@/components/ui';
 
 export default function UIPage() {
 	return (
@@ -16,6 +18,21 @@ export default function UIPage() {
 					<Sticker $success>Success</Sticker>
 				</Flexbox>
 			</LayerBlock>
+			<LayerBlock>
+				<h2>Buttons</h2>
+				<Flexbox $gap={10}>
+					<Button>Default</Button>
+					<Button $primary>Primary</Button>
+					<Button $danger>Danger</Button>
+					<Button $dark>Dark</Button>
+					<Button $success>Success</Button>
+				</Flexbox>
+			</LayerBlock>
+
+			<h2>Product</h2>
+			<Grid>
+				<ProductCard product={mockProduct} />
+			</Grid>
 		</Container>
 	);
 }
---
+++ components/ProductCard/mockProduct.ts
@@ -0,0 +1,18 @@
+export const mockProduct = {
+	id: 6,
+	title: 'Calvin Klein CK One',
+	description:
+		"CK One by Calvin Klein is a classic unisex fragrance, known for its fresh and clean scent. It's a versatile fragrance suitable for everyday wear.",
+	category: 'fragrances',
+	price: 49.99,
+	discountPercentage: 0.32,
+	rating: 4.85,
+	tags: ['fragrances', 'perfumes'],
+	brand: 'Calvin Klein',
+	images: [
+		'https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/1.png',
+		'https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/2.png',
+		'https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/3.png',
+	],
+	thumbnail: 'https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/thumbnail.png',
+};
